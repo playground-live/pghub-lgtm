3e1e50249d64047f02ccfb4d7b167c3b5cb84758
Ebinuma Kenichi <ktennis.mqekr12@gmail.com>
2017-10-02 20:28:22 +0900

    add valid_md_link? method (wip)

--- lib/pghub/lgtm.rb
+++ lib/pghub/lgtm.rb
@@ -1,34 +1,46 @@
-require "pghub/lgtm/version"
-require "mechanize"
+require 'pghub/lgtm/version'
+require 'mechanize'
 
 module PgHub
   module Lgtm
-    LGTM_MARKDOWN_PATTERN = /(!\[LGTM\]\(.+\))\]/
-
     class << self
       def post_to(issue_path)
-        text = markdown_lgtm_from('http://lgtm.in')
-
-        if text =~ LGTM_MARKDOWN_PATTERN
-          image_md_link = Regexp.last_match[1]
-          # image_md_linkが正常なURLか判定する
-          comment_client = GithubAPI.new(issue_path)
-          comment_client.post(image_md_link)
-        else
-          raise 'Invalid text near "LGTM"'
-        end
+        md_link = md_link_from('http://lgtm.in')
+        comment_client = GithubAPI.new(issue_path)
+        comment_client.post(md_link)
       end
 
       private
 
-      def markdown_lgtm_from(url)
+      def md_link_from(url)
         agent = Mechanize.new
 
-        raise 'Random button is not found in http://lgtm.in.' unless agent.get(url).link_with(text: 'Random')
-        page = agent.get(url).link_with(text: 'Random').click
+        5.times do |i|
+          raise 'Random button is not found in http://lgtm.in.' unless agent.get(url).link_with(text: 'Random')
+          page = agent.get(url).link_with(text: 'Random').click
 
-        raise 'Markdown text is not found in http://lgtm.in.' unless page.at('textarea#markdown')
-        page.at('textarea#markdown').inner_text
+          raise 'Markdown text is not found in http://lgtm.in.' unless page.at('input#dataUrl')
+          url = page.at('input#dataUrl').inner_text
+
+          break if valid_md_link?("http://lgtm.in/i/0KN4MtJRp")
+
+          raise 'There are not valid URL' if i == 4
+        end
+
+        "![lgtm](#{url})"
+      end
+
+      def valid_md_link?(url)
+        connection = Faraday.new(url: url) do |faraday|
+          faraday.request :multipart
+          faraday.request :url_encoded
+          faraday.response :raise_error
+          faraday.adapter :net_http
+        end
+        connection.get
+        return true
+      rescue
+        return false
       end
     end
   end
