131eb1aa6f2241fddb5665db56751d1c69a7ff4d
Ebinuma Kenichi <ktennis.mqekr12@gmail.com>
2017-10-03 18:55:10 +0900

    refactoring

--- lib/pghub/lgtm.rb
+++ lib/pghub/lgtm.rb
@@ -5,20 +5,18 @@ module PgHub
   module Lgtm
     class << self
       def post_to(issue_path)
-        md_link = md_link_from('http://lgtm.in/g')
         comment_client = GithubAPI.new(issue_path)
-        comment_client.post(md_link)
+        comment_client.post(get_md_link)
       end
 
       private
 
-      def md_link_from(url)
-        agent = Mechanize.new
-        agent.redirect_ok = true
+      def get_md_link
+        agent = Mechanize.new(redirect_ok: true)
         img_url = ''
 
         3.times do |i|
-          page = agent.get(url)
+          page = agent.get('http://lgtm.in/g')
 
           raise 'Markdown URL is not found in http://lgtm.in/g.' unless page.at('input#dataUrl')
           img_url = page.at('input#dataUrl')[:value].gsub(%r{\/i\/}, '/p/')
@@ -32,16 +30,16 @@ module PgHub
       end
 
       def valid_redirect_link?(url)
-        redirect_url = redirect_from(url)
-        response = Net::HTTP.get_response(URI.parse(redirect_url))
+        redirect_url = get_response(url)['location']
+        response = get_response(redirect_url)
       rescue
         return false
       else
         return response.code == '200' ? true : false
       end
 
-      def redirect_from(url)
-        Net::HTTP.get_response(URI.parse(url))['location']
+      def get_response(url)
+        Net::HTTP.get_response(URI.parse(url))
       end
     end
   end
