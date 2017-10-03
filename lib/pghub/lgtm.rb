df205c102cdccd0770450cdb56c51051505c3de2
Ebinuma Kenichi <ktennis.mqekr12@gmail.com>
2017-10-03 16:47:51 +0900

    delete click method and setup redirect method

--- lib/pghub/lgtm.rb
+++ lib/pghub/lgtm.rb
@@ -5,7 +5,7 @@ module PgHub
   module Lgtm
     class << self
       def post_to(issue_path)
-        md_link = md_link_from('http://lgtm.in')
+        md_link = md_link_from('http://lgtm.in/g')
         comment_client = GithubAPI.new(issue_path)
         comment_client.post(md_link)
       end
@@ -14,38 +14,34 @@ module PgHub
 
       def md_link_from(url)
         agent = Mechanize.new
+        agent.redirect_ok = true
         img_url = ''
 
-        5.times do |i|
-          raise 'Random button is not found in http://lgtm.in.' unless agent.get(url).link_with(text: 'Random')
-          page = agent.get(url).link_with(text: 'Random').click
+        3.times do |i|
+          page = agent.get(url)
 
-          raise 'Markdown text is not found in http://lgtm.in.' unless page.at('input#dataUrl')
-          img_url = convert_into_img_link(page.at('input#dataUrl')[:value])
-          redirect_url = redirect_from(img_url)
+          raise 'Markdown URL is not found in http://lgtm.in/g.' unless page.at('input#dataUrl')
+          img_url = page.at('input#dataUrl')[:value].gsub(%r{\/i\/}, '/p/')
 
-          break if valid_link?(redirect_url)
+          break if valid_redirect_link?(img_url)
 
-          raise 'Invalid URL' if i == 4
+          raise "Many invalid URLs in #{url}" if i == 2
         end
 
         "![lgtm](#{img_url})"
       end
 
-      def convert_into_img_link(link)
-        link.gsub(%r{\/i\/}, '/p/')
-      end
-
-      def redirect_from(url)
-        Net::HTTP.get_response(URI.parse(url))['location']
-      end
-
-      def valid_link?(url)
-        response = Net::HTTP.get_response(URI.parse(url))
+      def valid_redirect_link?(url)
+        redirect_url = redirect_from(url)
+        response = Net::HTTP.get_response(URI.parse(redirect_url))
       rescue
         return false
       else
         return response.code == '200' ? true : false
+      end
+
+      def redirect_from(url)
+        Net::HTTP.get_response(URI.parse(url))['location']
       end
     end
   end
